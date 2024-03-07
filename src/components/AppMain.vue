aa900be38fbe1b0d929f419ba0c5c5771d60d3ce
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-03-07 23:38:30 +0100

    Creato lo store inserendo un endpoint e fatto la prima chiamata Api

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,32 +1,38 @@
 <script>
+import AppPost from './AppPost.vue';
+import { store } from '../store.js';
+import axios from 'axios';
+
 export default {
+    name: 'AppMain',
+    components: {
+        AppPost 
+    },
     data() {
         return {
-         
-            
+            store,
+            posts:[],
+
         }
     },
     methods: {
-        
-    
+       
     },
     created() {
-       
+        axios.get(this.store.ApiPosts)
+        .then(response => {
+            console.log(response.data.results);
+        })
     }
 }
-
-
 </script>
-<template lang="">
-    <main>
-        <h1 class="text-center"> Prova main</h1>
+
+<template>
+    <main class="container">
+        <AppPost /> 
     </main>
-    
-
 </template>
-<style lang ="scss" scoped>
-    h1{
-        color: aquamarine;
-    }
-</style>
 
+<style scoped>
+/* Aggiungi i tuoi stili locali qui */
+</style>
---
+++ src/store.js
@@ -0,0 +1,5 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+    ApiPosts: 'http://127.0.0.1:8000/api/posts'
+})
